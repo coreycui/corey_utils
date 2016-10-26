df8f8ba740c615f980834a89db74205506bc7528
coreycui <coreymylife@163.com>
2016-10-26 10:28:07 +0800

    add fast close socket util class

---
+++ src/org/corey/expiryqueue/FastCloseSockUtil.java
@@ -0,0 +1,18 @@
+package org.corey.expiryqueue;
+
+import java.net.SocketException;
+import java.nio.channels.SocketChannel;
+
+public class FastCloseSockUtil {
+	public static void fastCloseSock(SocketChannel sc) {
+        if (sc != null) {
+            try {
+                // Hard close immediately, discarding buffers
+                sc.socket().setSoLinger(true, 0);
+            } catch (SocketException e) {
+                //LOG.warn("Unable to set socket linger to 0, socket close"
+                //         + " may stall in CLOSE_WAIT", e);
+            }
+        }
+    }
+}
